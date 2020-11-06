c77e77bf0ac97da40bb6894ef2daa8d9932a2099
Ceci2003 <progplay2003@gmail.com>
2020-11-07 00:32:09 +0200

    Updated not logged views: Home/Index.cshtml, Login.cshtml, Register.cshtml

--- Web/BeekeeperAssistant.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Web/BeekeeperAssistant.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace BeekeeperAssistant.Web.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Запомни ме?")]
             public bool RememberMe { get; set; }
         }
 

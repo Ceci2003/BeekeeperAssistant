cd213fa609098c6a01c5e58c055dadb7a3193871
Ivan Angelov <61803518+ivan4o03@users.noreply.github.com>
2020-11-17 16:08:07 +0200

    Add queen controller and views.

---
+++ Web/BeekeeperAssistant.Web/Controllers/QueenController.cs
@@ -0,0 +1,28 @@
+﻿namespace BeekeeperAssistant.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class QueenController : Controller
+    {
+        public IActionResult All()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string putAnInputModel)
+        {
+            return this.Redirect("/");
+        }
+    }
+}

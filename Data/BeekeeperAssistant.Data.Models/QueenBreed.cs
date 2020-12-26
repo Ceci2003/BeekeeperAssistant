d51db98f664418b77cd5ef1d1f6782afae2761e1
Ceci2003 <ceci1706@abv.bg>
2020-12-26 20:52:03 +0200

    Update Queen enumerations

--- Data/BeekeeperAssistant.Data.Models/QueenBreed.cs
+++ Data/BeekeeperAssistant.Data.Models/QueenBreed.cs
@@ -1,6 +1,11 @@
 ﻿namespace BeekeeperAssistant.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum QueenBreed
     {
+         StripedCarniolanQueen = 1,
+         CordovanItalianQueen = 2,
+         ItalianQueen = 3,
     }
 }

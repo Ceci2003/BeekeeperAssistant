22b37ac30c937b7993c09d8783297a12fe14bbfe
Ivan Mihaylov <ivan4o03.it@gmail.com>
2021-04-10 13:44:50 +0300

    Add edit beehive and display all beehives

--- Services/BeekeeperAssistant.Services.Data/BeehiveService.cs
+++ Services/BeekeeperAssistant.Services.Data/BeehiveService.cs
@@ -1,6 +1,7 @@
 ﻿namespace BeekeeperAssistant.Services.Data
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -42,12 +43,44 @@
 
         public async Task<string> DeleteBeehiveByIdAsync(int beehiveId)
         {
-            var beehive = this.beehiveRepository.All().Include(b => b.Apiary).FirstOrDefault(b => b.Id == beehiveId);
+            var beehive = this.beehiveRepository.All()
+                .Include(b => b.Apiary)
+                .FirstOrDefault(b => b.Id == beehiveId);
 
             this.beehiveRepository.Delete(beehive);
             await this.beehiveRepository.SaveChangesAsync();
 
             return beehive.Apiary.Number;
+        }
+
+        public async Task<int> EditUserBeehiveAsync(int beehiveId, int number, BeehiveSystem beehiveSystem, BeehiveType beehiveType, DateTime dateTime, int apiaryId, BeehivePower beehivePower, bool hasDevice, bool hasPolenCatcher, bool hasPropolisCatcher)
+        {
+            var beehive = this.beehiveRepository.All()
+                .FirstOrDefault(b => b.Id == beehiveId);
+
+            beehive.Number = number;
+            beehive.BeehiveSystem = beehiveSystem;
+            beehive.BeehiveType = beehiveType;
+            beehive.BeehivePower = beehivePower;
+            beehive.Date = dateTime;
+            beehive.ApiaryId = apiaryId;
+            beehive.HasDevice = hasDevice;
+            beehive.HasPolenCatcher = hasPolenCatcher;
+            beehive.HasPropolisCatcher = hasPropolisCatcher;
+
+            await this.beehiveRepository.SaveChangesAsync();
+
+            return beehive.Id;
+        }
+
+        public IEnumerable<T> GetAllUserBeehives<T>(string userId)
+        {
+            var allUserBeehives = this.beehiveRepository.All()
+                .Where(b => b.CreatorId == userId)
+                .To<T>()
+                .ToList();
+
+            return allUserBeehives;
         }
 
         public T GetBeehiveById<T>(int beehiveId)

bbeefbb6c0ed5d24a066ccbb48f5b72fbcd29f63
Ceci2003 <ceci1706@abv.bg>
2021-04-16 02:35:28 +0300

    Add breadcrumb Add it to apiaries

---
+++ Data/BeekeeperAssistant.Data/Migrations/20210415224959_QueensKey.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeekeeperAssistant.Data.Migrations
+{
+    public partial class QueensKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

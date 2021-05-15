abb88a4c7f870b04d2c7f9ac6dc8a6f2e2ad2335
Ivan Mihaylov <ivan4o03.it@gmail.com>
2021-05-15 14:22:53 +0300

    Add moment.js Make time on input forms today

--- Web/BeekeeperAssistant.Web/wwwroot/js/site.js
+++ Web/BeekeeperAssistant.Web/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿// Write your JavaScript code.
+
+﻿$(function () {
+    $("time").each(function (i, e) {
+        const dateTimeValue = $(e).attr("datetime");
+        if (!dateTimeValue) {
+            return;
+        }
+
+        const time = moment.utc(dateTimeValue).local();
+        $(e).html(time.format("llll"));
+        $(e).attr("title", $(e).attr("datetime"));
+    });
+});

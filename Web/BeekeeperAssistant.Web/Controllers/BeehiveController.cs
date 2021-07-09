70f26b2631e78418dea1ab3dbe7eca57cfbc72c4
Ceci2003 <progplay2003@gmail.com>
2021-07-09 22:57:53 +0300

    Added ExportToExcel to all Beehives

--- Web/BeekeeperAssistant.Web/Controllers/BeehiveController.cs
+++ Web/BeekeeperAssistant.Web/Controllers/BeehiveController.cs
@@ -1,17 +1,22 @@
 ﻿namespace BeekeeperAssistant.Web.Controllers
 {
     using System;
+    using System.Collections.Generic;
+    using System.Drawing;
     using System.Linq;
     using System.Threading.Tasks;
 
     using BeekeeperAssistant.Common;
     using BeekeeperAssistant.Data.Models;
     using BeekeeperAssistant.Services.Data;
+    using BeekeeperAssistant.Web.ViewModels.Apiaries;
     using BeekeeperAssistant.Web.ViewModels.Beehives;
     using BeekeeperAssistant.Web.ViewModels.Harvest;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using OfficeOpenXml;
+    using OfficeOpenXml.Style;
 
     [Authorize]
     public class BeehiveController : BaseController
@@ -170,6 +175,91 @@
 
             var apiaryNumber = await this.beehiveService.DeleteBeehiveByIdAsync(id);
             return this.Redirect($"/Apiary/{apiaryNumber}");
+        }
+
+        public async Task<IActionResult> ExportToExcel(int? id)
+        {
+            // ToDo: Add Export by apiary Id
+            var currentUser = await this.userManager.GetUserAsync(this.User);
+            var beehives = this.beehiveService.GetAllUserBeehives<BeehiveDataViewModel>(currentUser.Id).ToList();
+
+            ExcelPackage pck = new ExcelPackage();
+            ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Report");
+
+            ws.Cells["A1:B1"].Merge = true;
+            ws.Cells["A1"].Value = "Доклад - Кошери";
+            ws.Cells["A2:B2"].Merge = true;
+            ws.Cells["A2"].Value = $"Дата: {string.Format("{0:dd-MM-yyyy} {0:H:mm}", DateTimeOffset.Now)}";
+
+            ws.Cells["A4"].Value = "Номер на кошера";
+            ws.Cells["B4"].Value = "Създаден на";
+            ws.Cells["C4"].Value = "Номер на пчелин";
+            ws.Cells["D4"].Value = "Сила";
+            ws.Cells["E4"].Value = "Система";
+            ws.Cells["F4"].Value = "Тип";
+            ws.Cells["G4"].Value = "Прашецоуловител";
+            ws.Cells["H4"].Value = "Решетка за прополис";
+
+            ws.Cells["I4"].Value = "Кралица";
+            ws.Cells["J4"].Value = "Кралица-Цвят";
+            ws.Cells["K4"].Value = "Кралица-Дата на придаване";
+            ws.Cells["L4"].Value = "Кралица-Произход";
+            ws.Cells["M4"].Value = "Кралица-Вид";
+            ws.Cells["N4"].Value = "Кралица-Порода";
+            ws.Cells["O4"].Value = "Кралица-Нрав";
+            ws.Cells["P4"].Value = "Кралица-Хигиенни навици";
+
+            ws.Cells["A4:P4"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            ws.Cells["A4:P4"].Style.Fill.BackgroundColor.SetColor(1, 183, 225, 205);
+            ws.Cells["A4:P4"].Style.Font.Color.SetColor(Color.White);
+
+            int rowIndex = 5;
+            foreach (var beehive in beehives)
+            {
+                ws.Cells[$"A{rowIndex}"].Value = beehive.Number;
+                ws.Cells[$"B{rowIndex}"].Value = beehive.Date.ToString("dd-MM-yyyy");
+                ws.Cells[$"C{rowIndex}"].Value = beehive.ApiaryNumber;
+                ws.Cells[$"D{rowIndex}"].Value = beehive.BeehivePower;
+                ws.Cells[$"E{rowIndex}"].Value = beehive.BeehiveSystem;
+                ws.Cells[$"F{rowIndex}"].Value = beehive.BeehiveType;
+                ws.Cells[$"G{rowIndex}"].Value = beehive.HasPolenCatcher == true ? "Да" : "Не";
+                ws.Cells[$"H{rowIndex}"].Value = beehive.HasPropolisCatcher == true ? "Да" : "Не";
+                if (beehive.HasQueen)
+                {
+                    //Color color = Color.White;
+                    //if (beehive.Queen.Color != )
+                    //{
+                    //    switch (beehive.Queen.Color)
+                    //    {
+                    //        case QueenColor.White: color = Color.White; break;
+                    //        case QueenColor.Yellow: color = Color.Yellow; break;
+                    //        case QueenColor.Red: color = Color.Red; break;
+                    //        case QueenColor.Green: color = Color.Green; break;
+                    //        case QueenColor.Blue: color = Color.Blue; break;
+                    //        default: color = Color.White; break;
+                    //    }
+                    //}
+
+                    //ws.Cells[$"J{rowIndex}"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    //ws.Cells[$"J{rowIndex}"].Style.Fill.BackgroundColor.SetColor(1, 183, 225, 205);
+
+                    ws.Cells[$"I{rowIndex}"].Value = beehive.HasQueen == true ? "Да" : "Не";
+                    ws.Cells[$"J{rowIndex}"].Value = "";
+                    ws.Cells[$"K{rowIndex}"].Value = beehive.Queen.GivingDate.ToString("dd-MM-yyyy");
+                    ws.Cells[$"L{rowIndex}"].Value = beehive.Queen.Origin;
+                    ws.Cells[$"M{rowIndex}"].Value = beehive.Queen.QueenType;
+                    ws.Cells[$"N{rowIndex}"].Value = beehive.Queen.Breed;
+                    ws.Cells[$"O{rowIndex}"].Value = beehive.Queen.Temperament;
+                    ws.Cells[$"P{rowIndex}"].Value = beehive.Queen.HygenicHabits;
+                }
+
+                rowIndex++;
+            }
+
+            ws.Cells["A:AZ"].AutoFitColumns();
+
+            this.Response.Headers.Add("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
+            return new FileContentResult(pck.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         }
     }
 }

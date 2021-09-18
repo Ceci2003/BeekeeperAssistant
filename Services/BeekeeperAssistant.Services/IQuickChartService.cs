9c971b62005e57a6d7e4e7fb8ab9dcc436d767d2
Ceci2003 <ceci1706@abv.bg>
2021-09-18 11:50:54 +0300

    Scaffolded Identity Layout. Added QuickChartService

---
+++ Services/BeekeeperAssistant.Services/IQuickChartService.cs
@@ -0,0 +1,11 @@
+﻿namespace BeekeeperAssistant.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IQuickChartService
+    {
+        string ImageUrl(string chartType, List<int> data, int height, int width);
+    }
+}
---
+++ Services/BeekeeperAssistant.Services/QuickChartService.cs
@@ -0,0 +1,38 @@
+﻿namespace BeekeeperAssistant.Services
+{
+    using System.Collections.Generic;
+
+    using QuickChart;
+
+    public class QuickChartService : IQuickChartService
+    {
+        public string ImageUrl(string chartType, List<int> data, int height = 300, int width = 500)
+        {
+            Chart chart = new Chart();
+
+            chart.Width = 500;
+            chart.Height = 300;
+            chart.Config = @"{
+						  type: 'pie',
+						  data: {
+						    datasets: [{ 
+						      data: [10, 20, 30]
+						    }]
+						  }
+						}";
+
+            chart.Config = "{ " +
+                                "type: 'pie', " +
+                                "data: { " +
+                                    "datasets: [{ " +
+                                        $"data: [{string.Join(", ", data)}] " +
+                                    "}] " +
+                                "} " +
+                            "}";
+
+            // Get the URL
+            var url = chart.GetUrl();
+            return url;
+        }
+    }
+}

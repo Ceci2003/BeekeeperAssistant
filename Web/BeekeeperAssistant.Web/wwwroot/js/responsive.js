f5e9598f5725dae2909b634f2c59826afb27a585
Ceci2003 <ceci1706@abv.bg>
2021-12-09 01:34:28 +0200

    Updated Apiary/ByNumber.cshtml responsiveness. Started Beehive/ById.cshtml responsiveness.

--- Web/BeekeeperAssistant.Web/wwwroot/js/responsive.js
+++ Web/BeekeeperAssistant.Web/wwwroot/js/responsive.js
@@ -10,31 +10,48 @@ if (screenWidth <= 1023) {
     allResponsiveTable.forEach(table => {
         table.style.display = 'block'
     });
-}
-else {
+
+    var tabs = document.querySelectorAll('.tablinks');
     
+    if (tabs.length > 0) {
+        var tabsWidth = 0;
+        tabs.forEach(tab => {
+            tabsWidth += tab.offsetWidth;
+        });
+
+        if (tabsWidth >= screenWidth) {
+            var tabWidth = screenWidth / tabs.length;
+            tabs.forEach(tab => {
+                var i = tab.children[0];
+                i.classList += ' m0';
+                
+                tab.innerHTML = i.outerHTML;
+                tab.style.width = tabWidth + 'px';
+            });
+        }
+    }
 }
 
-// window.addEventListener('resize', function(event){
-//     var screenWidth = screen.width;
+if (screenWidth <= 767) {
 
-//     var allTable = document.querySelectorAll(".all-table");
-//     var allResponsiveTable = document.querySelectorAll(".all-table-responsive");
-//     if (screenWidth <= 1023) {
-//         console.log(screenWidth);
-//         allTable.forEach((table) => {
-//             table.style.display = "none";
-//         });
-//         allResponsiveTable.forEach((table) => {
-//             table.style.display = "block";
-//         });
-//     } else {
-//         console.log(screenWidth);
-//         allTable.forEach((table) => {
-//             table.style.display = "table";
-//         });
-//         allResponsiveTable.forEach((table) => {
-//             table.style.display = "none";
-//         });
-//     }
-// });
+    // style tab links
+    var tabs = document.querySelectorAll('.tablinks');
+    
+    if (tabs.length > 0) {
+        var tabsWidth = 0;
+        tabs.forEach(tab => {
+            tabsWidth += tab.offsetWidth;
+        });
+
+        if (tabsWidth >= screenWidth) {
+            var tabWidth = screenWidth / tabs.length;
+            tabs.forEach(tab => {
+                var i = tab.children[0];
+                i.classList += ' m0';
+
+                tab.innerHTML = i.outerHTML;
+                tab.style.width = tabWidth + 'px';
+            });
+        }
+    }
+}

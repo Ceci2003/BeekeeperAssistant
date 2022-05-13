c54074446c623ae6406be7fc36b9fde374c8e0ce
Ceci2003 <ceci1706@abv.bg>
2022-05-13 14:51:40 +0300

    Fix bug in filter class for data filtration

--- BeekeeperAssistant.Data.Filters/Filter.cs
+++ BeekeeperAssistant.Data.Filters/Filter.cs
@@ -9,24 +9,44 @@ using BeekeeperAssistant.Services.Mapping;
 
 namespace BeekeeperAssistant.Data.Filters
 {
-    public class Filter<T> : IFilter<T> where T: IDefaultOrder<T>, new()
+    public class Filter<T> : IFilter<T> where T : IDefaultOrder<T>, new()
     {
         public IOrderedQueryable<T> FilterCollection(IQueryable<T> query, FilterModel filterModel)
         {
-            if (filterModel != null)
-            {
-                switch (filterModel.Order)
-                {
-                    case Order.Ascending:
-                        return query.OrderByProeprty(filterModel.PropertyName);
-                    case Order.Descending:
-                        return query.OrderByProeprtyDescending(filterModel.PropertyName);
-                }
-            }
-
             var tType = new T();
 
+            if (filterModel != null)
+            {
+                if (this.HasProperty(typeof(T), filterModel.PropertyName))
+                {
+                    switch (filterModel.Order)
+                    {
+                        case Order.Ascending:
+                            return query.OrderByProeprty(filterModel.PropertyName);
+                        case Order.Descending:
+                            return query.OrderByProeprtyDescending(filterModel.PropertyName);
+                    }
+                }
+
+            }
+
+
             return tType.DefaultOrder(query);
+        }
+
+        private bool HasProperty(Type type, string propertyName)
+        {
+            if (propertyName == null)
+            {
+                return false;
+            }
+
+            if (type.GetProperty(propertyName) == null)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }

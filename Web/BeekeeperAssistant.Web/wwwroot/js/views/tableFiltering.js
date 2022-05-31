905659a5d6e09b93713f028943ff661452b3a63a
Ceci2003 <ceci1706@abv.bg>
2022-05-31 19:43:38 +0300

    Updated design

--- Web/BeekeeperAssistant.Web/wwwroot/js/views/tableFiltering.js
+++ Web/BeekeeperAssistant.Web/wwwroot/js/views/tableFiltering.js
@@ -3,14 +3,22 @@ window.onload = hideFilter();
 function hideFilter() {
   var filter = document.getElementById("order");
   var column = document.getElementById("column");
+  var input = document.getElementById("input");
 
   if (filter.selectedIndex == 0) {
     column.classList.add("slide-bck-left");
     column.classList.remove("slide-right");
-    
-    document.getElementById("input").classList.add("slide-bck-left-btn");
+
+    if (window.innerWidth < 767) {
+      input.classList.add("up");
+    }
+    if (window.innerWidth > 767) {
+      input.classList.add("up");
+    }
   } else {
     column.classList.remove("slide-bck-left");
     column.classList.add("slide-right");
+
+    input.classList.remove("up");
   }
 }
